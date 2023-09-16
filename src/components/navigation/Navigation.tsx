13864f7da67b9226be3ece53da255618f3fbe23c
Tarik Selimovic <tariksss11@gmail.com>
2023-09-16 21:00:20 +0200

    Fix(drawer): changed designe of props displayed in drawer

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -100,12 +100,22 @@ export const PageMobileNav = ({
 						sx={{
 							flexDirection: 'column',
 							paddingTop: '2rem',
+							paddingLeft: '1rem',
+							paddingRight: '1rem',
 						}}
 					>
 						{blockLinks.map((link: React.ReactNode, index: number) => (
 							<NavDrawerLinkContainer
 								key={`loom-link-drawer-${index}`}
 								className="loom-header_nav-links-drawer-item"
+								sx={{
+									width: '100%',
+									display: 'flex',
+									justifyContent: 'center',
+									backgroundColor: 'white',
+									padding: '4px',
+									borderRadius: '0.5rem',
+								}}
 							>
 								{createElement((link as React.ReactElement).type, {
 									...(link as React.ReactElement).props,
